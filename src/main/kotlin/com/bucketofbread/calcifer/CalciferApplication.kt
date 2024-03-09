eb1be64eb9597d82e640d2905f461163b4cd6bef
cranemont <cranemont@gmail.com>
2024-03-09 16:50:46 +0900

    initial commit

---
+++ src/main/kotlin/com/bucketofbread/calcifer/CalciferApplication.kt
@@ -0,0 +1,11 @@
+package com.bucketofbread.calcifer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalciferApplication
+
+fun main(args: Array<String>) {
+    runApplication<CalciferApplication>(*args)
+}
---
+++ src/test/kotlin/com/bucketofbread/calcifer/CalciferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bucketofbread.calcifer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalciferApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
